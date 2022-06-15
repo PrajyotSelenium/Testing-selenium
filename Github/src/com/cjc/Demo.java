83425cd7ef69a71051268f29ea7523caeb4766c5
PrajyotSelenium <90840446+PrajyotSelenium@users.noreply.github.com>
2022-06-15 16:24:57 +1030

    by CJC

--- Github/src/com/cjc/Demo.java
+++ Github/src/com/cjc/Demo.java
@@ -1,7 +1,10 @@
 package com.cjc;
 
 public class Demo {
-
+        public void m1()
+	{
+		System.out.println("m1-method");
+	}	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Git hub");
